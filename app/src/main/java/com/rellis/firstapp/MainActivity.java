55f1589bee8c008d11738f7e9c14c5e84f23f9e0
Unknown <cubemaster365@gmail.com>
2017-07-20 16:55:52 -0500

    First

---
+++ app/src/main/java/com/rellis/firstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rellis.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void getHelloWorldText (View view){
+        Log.i("info", "ButtonPress");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
